f203c1359de1a593e9e2b06aa42c6e54b4fb0b76
Woyneshet Bilihatu <58231550+woynshetb@users.noreply.github.com>
2021-10-19 16:48:53 +0300

    Update Posts.js

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -4,15 +4,15 @@ import {Grid,CircularProgress} from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post.js'
-import useStyles from './styles.js';
+
 const Posts = ({setCurrentId}) =>{
     const posts = useSelector((state)=>state.posts);
-    const classes = useStyles();
+    
     console.log(posts);
     return(
         // if there is nothing inside the list(employee) sow circularprogress else make a grid to show or to list
       !posts.length ? <CircularProgress/> : (
-          <Grid className={classes.container} container alignItems="strech" spacing={3}>
+          <Grid container alignItems="strech" spacing={3}>
               {
                   posts.map((post)=>(
                       <Grid key={post._id} item xs={12} sm={6}>
